e1faa4e462e41d4ac2434e4cbdaabccd45f43b7b
woksin <sindrewilting@gmail.com>
2021-02-18 11:18:28 +0100

    Extension method for creating public backup filter

--- Source/Backup/Filters/FiltersBuilderExtensions.cs
+++ Source/Backup/Filters/FiltersBuilderExtensions.cs
@@ -1,28 +1,28 @@
 ﻿// Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Threading.Tasks;
+using Dolittle.Platform.Backup.Events;
+using Dolittle.SDK.Events;
 using Dolittle.SDK.Events.Filters;
 
 namespace Dolittle.Platform.Backup.Filters
 {
     public static class EventFiltersBuilderExtensions
     {
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="builder"></param>
-        /// <returns></returns>
-        public static EventFiltersBuilder BuildBackupStream(this EventFiltersBuilder builder)
+        public const string BackupFilterId = "584546c2-d3be-40ca-8321-d23dc7ed1d65";
+
+        public static EventFiltersBuilder CreateBackupFilter(this EventFiltersBuilder builder)
             => builder.CreatePublicFilter(
-                "584546c2-d3be-40ca-8321-d23dc7ed1d65",
-                _ => _.Handle((@event, context) =>
-                {
-                    
-                    return new PartitionedFilterResult(IsBackupEvent(), context.CommittedExecutionContext.Tenant.Value);
-                }));
+                BackupFilterId,
+                _ => _.Handle((@event, context) => Task.FromResult(new PartitionedFilterResult(IsBackupEvent(context.Type), context.CommittedExecutionContext.Tenant.Value))));
 
 
-        bool IsBackupEvent()
-            => 
+        static bool IsBackupEvent(EventType type)
+            => type.Id.Value.ToString() switch
+            {
+                EventTypeRegistry.BackupStartedId => true,
+                _ => false
+            };
     }
 }
